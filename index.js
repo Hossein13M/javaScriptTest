c02f572a7f6d6ebbbca103de48c91cf4619b25b9
Hossein Mousavi <hossein13mousavi@gmail.com>
2019-06-20 11:27:15 +0430

    Mosh exercises

--- index.js
+++ index.js
@@ -1,22 +1,227 @@
-"use strict"
-// so let's do this now!
-// let's do another one
-function Person(fName, lName, age, height, weight) {
-    this.firstName = fName;
-    this.lastName = lName;
-    this.height = height;
-    this.weight = weight;
-    this.stateChecker = function(money, mentalHealth) {
-        const Earnedmoney = money;
-        const healthState = mentalHealth;
-        if (Earnedmoney > 100) {
-            console.log('you are rich');
-        } else {
-            console.log('you are fucked up');
-        }
-    };
-    console.log("your name is:" + fName + ' ' + lName);
-    console.log('and you are: ' + height + 'cm and ' + weight + 'kg')
-};
-const person = new Person('hossein', 'mousavi', 20, 150, 90);
-const stats = person.stateChecker(10, "good");
+// dastan: template for declaring an object:
+// const test = {
+//     color: "black",
+// };
+
+
+
+// dastan: this was the first training of mine
+// "use strict"
+// // so let's do this now!
+// // let's do another one
+// function Person(fName, lName, age, height, weight) {
+//     this.firstName = fName;
+//     this.lastName = lName;
+//     this.height = height;
+//     this.weight = weight;
+//     this.stateChecker = function(money, mentalHealth) {
+//         const Earnedmoney = money;
+//         const healthState = mentalHealth;
+//         if (Earnedmoney > 100) {
+//             console.log('you are rich');
+//         } else {
+//             console.log('you are fucked up');
+//         }
+//     };
+//     console.log("your name is:" + fName + ' ' + lName);
+//     console.log('and you are: ' + height + 'cm and ' + weight + 'kg')
+// };
+// const person = new Person('hossein', 'mousavi', 20, 150, 90);
+// const stats = person.stateChecker(10, "good");
+
+// dastan: this is the second training of mine
+
+
+
+// dastan: exercise number1:
+
+// const address = function(st, ct, zc) {
+//     this.st = st;
+//     this.ct = ct;
+//     this.zc = zc;
+// }
+
+// address.prototype.showDirection = function(add) {
+//     console.log(add.st, " ", this.st);
+// }
+// address.prototype.showAddress = function() {
+//     console.log(this.st, " ", this.ct, " ", this.zc, " ");
+// }
+
+// address.prototype.showStreet = function() {
+//     console.log("street : ", this.st);
+// }
+
+// const add = new address("a", "b", "c");
+// console.log(add);
+// add.showAddress();
+// add.showStreet();
+// let newAddress = new address("z", "x", "y");
+// newAddress.showAddress();
+// console.log(newAddress);
+// add.showDirection(newAddress);
+
+// console.log(add.showDirection === newAddress.showDirection);
+// console.log(add.showAddress === newAddress.showAddress);
+
+
+// class g {
+//     test() {
+//         console.log("?");
+//     }
+// }
+
+// let gg = new g();
+// let ggg = new g();
+// gg.test();
+
+// console.log(g.prototype);
+
+// console.log(gg.test === ggg.test);
+
+// // const addObj = {
+// //     function showAddress(st, ct, zc) {
+
+// //     };
+// // };
+
+
+// dastan: test number one:
+// TODO: I need to come back at check this code
+
+
+
+// let addressObj = {
+//     st: "",
+//     ct: "",
+//     zc: "",
+//     showAddress(st, ct, zc) {
+//         this.st = st;
+//         this.st = st;
+//         this.st = st;
+//         console.log(`here is your address: ${st} ${ct} ${zc}`)
+//     },
+// }
+
+//dastan: exercise number 1:
+
+
+// let address = {
+//     street: "shademan",
+//     city: "tehran",
+//     zipCode: 103,
+// }
+
+// function showaddress() {
+//     for (const key in address) {
+//         console.log(key);
+
+
+//     }
+// }
+
+// showaddress();
+
+
+// dastan: exercise number2:
+
+
+// TODO: factory function:
+
+// let address = {
+//     street: "shademan",
+//     city: "tehran",
+//     zipCode: 103,
+// }
+
+// function showaddress(address) {
+//     console.log(address.street + address.city + address.zipCode);
+
+// }
+
+// let myAddress = new showaddress(address);
+
+// const proto = {
+//     drive(name) {
+//         console.log('vroom!!!');
+//     }
+// }
+
+// function createCar() {
+//     return Object.create(proto);
+// }
+
+// var a = createCar()
+// console.log(a)
+// Object.create(a)
+// proto.drive('car')
+
+// class test {
+//     constructor(name) {
+//         this.name = name + " 123 ";
+//     }
+//     showName() {
+//         console.log(this.name)
+//     }
+//     showThis() {
+//         console.log(this)
+//         return this
+//     }
+// }
+
+// var a = new test('ali')
+// console.log(a.showThis())
+
+// function test(name) {
+//     this.name = name;
+//     this.showName = function () {
+//         console.log(this.name)
+
+//     }
+
+// // }
+
+// var obj = {
+//     show: function() {
+//         return this;
+//     }
+// }
+
+// console.log(obj.show())
+// var b = new Object(obj)
+// console.log(b)
+
+// var c = Object.create(obj)
+// console.log(c)
+// console.log(c === b)
+
+
+// this is the exercise number 3:
+
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+
+}
+
+let address1 = new Address('a', 'b', 'c');
+let address2 = new Address('a', 'b', 'c');
+
+function areEqaul(firstAddress, secondAddress) {
+
+    if (firstAddress.street === secondAddress.street && firstAddress.city === secondAddress.city && firstAddress.zipCode === secondAddress.zipCode) {
+        console.log("they are the same address")
+    } else
+        console.log("these two addresses are not equal")
+}
+areEqaul(address1, address2);
+
+function areSame(add1, add2) {
+
+    if (add1 === add2) {
+        console.log("the are exactly the same")
+    } else
+        console.log("they are not exactly the same")
+}
+areSame(address1, address2);
